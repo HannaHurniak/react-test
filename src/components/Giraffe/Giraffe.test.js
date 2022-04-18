9eb0332b94207bcdc69015094d801a252a5e0fdd
Hanna Yermakovich <hanna.yermakovich@leverx.com>
2022-04-18 17:06:38 +0300

    fix: refactoring code

--- src/components/Giraffe/Giraffe.test.js
+++ src/components/Giraffe/Giraffe.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Giraffe from './Giraffe'
-import { FormattedMessage, useIntl } from 'react-intl';
 import "@testing-library/jest-dom/extend-expect" 
 
 it('should equal to 0', () => {
