a321ef3f48321fc8ae163cf9de887b25fc793c66
Hanna Yermakovich <hanna.yermakovich@leverx.com>
2022-04-18 16:40:14 +0300

    feat: add unit test

--- src/components/Giraffe/Giraffe.js
+++ src/components/Giraffe/Giraffe.js
@@ -1,15 +1,22 @@
-import { FormattedMessage, useIntl } from 'react-intl';
+// import { FormattedMessage, useIntl } from "react-intl";
 
 import giraffe from "./../../assets/giraffe.jpg";
 
 const Giraffe = () => {
-  const intl = useIntl();
+  // const intl = useIntl();
 
   return (
+    // <div>
+    //     <h2 data-testid="name"><FormattedMessage id={'text_giraffe'} values={{  }}/></h2>
+    //     <img src={giraffe} />
+    //     <h3>{intl.formatMessage({id: 'name_giraffe'})}</h3>
+    // </div>
     <div>
-        <h2><FormattedMessage id={'text_giraffe'} values={{  }}/></h2>
-        <img src={giraffe} />
-        <h3>{intl.formatMessage({id: 'name_giraffe'})}</h3>
+      <h2 data-testid="name">
+        I am a Giraffe!
+      </h2>
+      <img src={giraffe} />
+      <h3>Giraffe</h3>
     </div>
   );
 };
