d16da46e00318fd7e3f2a01d79e3545337e53c59
Hanna Yermakovich <hanna.yermakovich@leverx.com>
2022-04-19 12:05:26 +0300

    add heroku

--- src/components/Giraffe/Giraffe.js
+++ src/components/Giraffe/Giraffe.js
@@ -12,11 +12,11 @@ const Giraffe = () => {
     //     <h3>{intl.formatMessage({id: 'name_giraffe'})}</h3>
     // </div>
     <div>
-      <h2 data-testid="name">
+      <h2 data-testid="who">
         I am a Giraffe!
       </h2>
       <img src={giraffe} />
-      <h3>Giraffe</h3>
+      <h3 data-testid="name">Giraffe</h3>
     </div>
   );
 };
--- src/components/Giraffe/Giraffe.test.js
+++ src/components/Giraffe/Giraffe.test.js
@@ -3,7 +3,8 @@ import { render } from '@testing-library/react';
 import Giraffe from './Giraffe'
 import "@testing-library/jest-dom/extend-expect" 
 
-it('should equal to 0', () => {
+it('should equal to "Giraffe" ', () => {
     const { getByTestId } = render(<Giraffe />);
-    expect(getByTestId('name')).toHaveTextContent('I am a Giraffe!')
+    expect(getByTestId('name')).toHaveTextContent('Giraffe')
+    expect(getByTestId('who')).toHaveTextContent('I am a Giraffe!')
 });
