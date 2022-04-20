2ddeb09fe7c43ec7e671f70ea037f51ba8cc89a1
Hanna Yermakovich <hanna.yermakovich@leverx.com>
2022-04-20 14:55:52 +0300

    feat: add hippo page

---
+++ src/components/hippo/Hippo.js
@@ -0,0 +1,15 @@
+
+const Hippo = () => {
+
+  return (
+    <div>
+      <h2 data-testid="who">
+        I am a Hippo!
+      </h2>
+      {/* <img src={giraffe} /> */}
+      <h3 data-testid="name">Hippo</h3>
+    </div>
+  );
+};
+
+export default Hippo;
